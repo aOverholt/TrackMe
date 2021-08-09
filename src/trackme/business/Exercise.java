caf97cd0367d4b4d26dfbb16ae6134641860b682
Andrew <Andrew@localhost>
2021-08-09 01:04:44 -0400

    - Added Exercise.java to match up with the Exercises table in the database

---
+++ src/trackme/business/Exercise.java
@@ -0,0 +1,45 @@
+package trackme.business;
+
+/**
+ *
+ * @author L. Andrew Overholt
+ */
+public class Exercise {
+
+    // PROPERTIES
+    private String name;
+    private String type;
+
+    
+    
+    // CONSTRUCTORS
+    public Exercise(String name, String type) {
+        this.name = name;
+        this.type = type;
+    }
+    
+    
+    // GETTERS & SETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
+   
+    // METHODS  
+
+    
+    
+    
+}
