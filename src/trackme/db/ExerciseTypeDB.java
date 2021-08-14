7ab00c2e96a1b3426788979c4009932dc3a15fdc
Andrew <Andrew@localhost>
2021-08-14 16:59:42 -0400

    - Added ExerciseTypeDB - Implemented DAO and auto-generated its methods

---
+++ src/trackme/db/ExerciseTypeDB.java
@@ -0,0 +1,37 @@
+package trackme.db;
+
+import java.util.List;
+import trackme.business.ExerciseType;
+
+/**
+ *
+ * @author L. Andrew Overholt
+ */
+public class ExerciseTypeDB implements DAO<ExerciseType>  {
+
+    @Override
+    public ExerciseType get(String code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<ExerciseType> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean add(ExerciseType t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(ExerciseType t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(ExerciseType t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
