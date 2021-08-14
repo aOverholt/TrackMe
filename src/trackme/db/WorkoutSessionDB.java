204b1698892dae261a695d1cb97da17a6484c3db
Andrew <Andrew@localhost>
2021-08-14 16:57:24 -0400

    - Added WorkoutSessionDB - Implemented DAO and auto-generated its methods

---
+++ src/trackme/db/WorkoutSessionDB.java
@@ -0,0 +1,37 @@
+package trackme.db;
+
+import java.util.List;
+import trackme.business.WorkoutSession;
+
+/**
+ *
+ * @author L. Andrew Overholt
+ */
+public class WorkoutSessionDB implements DAO<WorkoutSession> {
+
+    @Override
+    public WorkoutSession get(String code) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<WorkoutSession> getAll() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean add(WorkoutSession t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean update(WorkoutSession t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean delete(WorkoutSession t) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
