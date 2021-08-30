e5a07dff93625f6526402b99477be6aa887d4cf5
Andrew <Andrew@localhost>
2021-08-30 17:44:57 -0400

    - Fixed some readability issues

---
+++ src/trackme/business/WorkoutSession.java
@@ -0,0 +1,142 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trackme.business;
+
+/**
+ *
+ * @author L. Andrew Overholt
+ */
+public class WorkoutSession {
+
+    // PROPERTIES
+    private int sessionID;
+    private String date;
+    private int exerciseID;
+    private Exercise exerciseName;
+    private String duration;
+    private int bodyWeight;
+    private int averageHeartRate;
+    private double distance;
+    private int weight;
+    private int sets;
+    private int reps;
+
+    
+    
+    // CONSTRUCTORS
+    /*
+     * TODO: add a method that gets the sessionID of the last object in the list of 
+     *       workout sessions, then adds 1 to it, then uses that for the next sessionID
+     * 
+     * TODO: add a method that gets the exercise name from the sessionID; might be better 
+     *       to do this in the Exercise class
+     */
+    public WorkoutSession(int sessionID, String date, int exerciseID,
+                          Exercise exerciseName, String duration, int bodyWeight,
+                          int averageHeartRate, double distance, int weight,
+                          int sets, int reps) {
+        this.sessionID = sessionID;
+        this.date = date;
+        this.exerciseID = exerciseID;
+        this.exerciseName = exerciseName;
+        this.duration = duration;
+        this.bodyWeight = bodyWeight;
+        this.averageHeartRate = averageHeartRate;
+        this.distance = distance;
+        this.weight = weight;
+        this.sets = sets;
+        this.reps = reps;
+    }
+    
+    
+    
+    // GETTERS
+    public int getSessionID() {
+        return sessionID;
+    }
+    public String getDate() {
+        return date;
+    }
+    public int getExerciseID() {
+        return exerciseID;
+    }
+    public Exercise getExerciseName() {
+        return exerciseName;
+    }
+    public String getDuration() {
+        return duration;
+    }
+    public int getBodyWeight() {
+        return bodyWeight;
+    }
+    public int getAverageHeartRate() {
+        return averageHeartRate;
+    }
+    public double getDistance() {
+        return distance;
+    }
+    public int getWeight() {
+        return weight;
+    }
+    public int getSets() {
+        return sets;
+    }
+    public int getReps() {
+        return reps;
+    }
+    
+    
+    
+    // SETTERS
+    public void setSessionID(int sessionID) {
+        this.sessionID = sessionID;
+    }
+    public void setDate(String date) {
+        this.date = date;
+    }
+    public void setExerciseID(int exerciseID) {
+        this.exerciseID = exerciseID;
+    }
+    public void setExerciseName(Exercise exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+    public void setBodyWeight(int bodyWeight) {
+        this.bodyWeight = bodyWeight;
+    }
+    public void setAverageHeartRate(int averageHeartRate) {
+        this.averageHeartRate = averageHeartRate;
+    }
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+    public void setSets(int sets) {
+        this.sets = sets;
+    }
+    public void setReps(int reps) {
+        this.reps = reps;
+    }
+    
+    
+    
+    // METHODS  
+
+    
+
+    
+
+    
+
+    
+
+    
+    
+}
